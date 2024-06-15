84503df695a6c3ae8006a5696d9f5491125fc333
Vandelet Marin <vandeletmarin.pro@gmail.com>
2024-06-15 14:41:07 +0200

    Add files via upload

--- gestion_utilisateurs.php
+++ gestion_utilisateurs.php
@@ -18,28 +18,10 @@ include "header.php";
         <button class="button-creer" onclick="location.href='new_user.php'">Créer un utilisateur</button>
         <button class="button-lister" onclick="location.href='list_users.php'">Lister les utilisateurs</button>
     </div>
-    <footer>
-        <img src="img/logouniv.png" alt="EDN_Logo" class="logo-univ">
-        <p>5 boulevard Descartes <br>
-            Champs-sur-Marne <br>
-            77454 Marne-la-Vallée cedex 2 <br>
-            Téléphone : +33 (0)1 60 95 75 00
-        </p>
+    <?php
 
-        <ul class="social-icons">
-            <li><a href="https://www.facebook.com/UniversiteGustaveEiffel/" class="icon"><img
-                        src="img/facebookicon.png"
-                        alt="icone facebook" class="icon"></a></li>
-            <li><a href="https://twitter.com/UGustaveEiffel" class="icon"><img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png"
-                        alt="icone twitter" class="icon"></a></li>
-            <li><a href="https://fr.linkedin.com/school/université-gustave-eiffel/" class="icon"><img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/72px-LinkedIn_icon.svg.png"
-                        alt="icone linkedin" class="icon"></a></li>
-            <li><a href="https://www.instagram.com/universitegustaveeiffel/" class="icon"><img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png"
-                        alt="icone instagram" class="icon"></a></li>
-        </ul>
-    </footer>
+    include "footer.php";
+
+    ?>
 </body>
 </html>
